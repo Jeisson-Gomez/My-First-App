01152813b2b3cee6f89b39d4f449d0921586b602
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-03-15 17:29:56 -0500

    Actualizacion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorld } from './primeromponente/primer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorld
   ],
   imports: [
     BrowserModule,
